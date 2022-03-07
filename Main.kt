132c29a8274e28c31d656e1dd8d5a8de7550b7e8
Redjive2 <60155382+zestyzimbabwan@users.noreply.github.com>
2022-03-07 16:28:27 -0600

    Update Main.kt

--- Main.kt
+++ Main.kt
@@ -1,12 +1,10 @@
 import inputCollector
 
 fun main() {
-
-    var input = inputCollector.readFile("(insert .txt file here)")
-    var output = inputCollector.tokenize(input, mainConfig)
+    var input = inputCollector.readFile("( insert .txt file here )")
+    var output = inputCollector.convertInput(input, mainConfig)
     println(output)
 
-    var config = inputCollector.newCustomConfig(mutableListOf(" ", ";", "\r\n"), ';', mutableListOf("/start/", "/end/"))
-    output = inputCollector.tokenize(input, config)
-
+    var config = inputCollector.newCustomConfig(mutableListOf(" ", ";", "\r\n"), ';', mutableListOf("/start/", "/end/",), mutableListOf(true, true, true))
+            output = inputCollector.convertInput(input, config)
 }
